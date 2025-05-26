bb080afb51f735f6621e60dba8b0393c9e95f38d
soufyane-felate <soufyanfellat03@gmail.com>
2025-05-26 10:33:20 +0100

    E-Tech-Store-Backend

---
+++ src/main/java/com/E_Tech_Store_Backend/E_Tech_Store_Backend/ETechStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.E_Tech_Store_Backend.E_Tech_Store_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ETechStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ETechStoreBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/E_Tech_Store_Backend/E_Tech_Store_Backend/ETechStoreBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.E_Tech_Store_Backend.E_Tech_Store_Backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ETechStoreBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
